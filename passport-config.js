96cefe475d21aca2c43f98c0f1ea5c1668a3044b
Bertin Mouya Madi <bmouyam@yahoo.fr>
2022-01-14 11:19:18 -0500

    ok

---
+++ passport-config.js
@@ -0,0 +1,33 @@
+const LocalStrategy = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+
+function initialize(passport, getUserByUsername, getUserById) {
+  const authenticateUser = async (username, password, done) => {
+    const user = getUserByUsername(username)
+
+    //Si aucune utilisateur a été trouvé avec le username reçu
+    if (user == null) {
+      return done(null, false, { message: "Ce nom d'utilisateur n'existe pas"})
+    }
+
+    try {
+      //Si le mot de passe reçu est bien celui de l'utilisateur associé au username reçu
+      if (await bcrypt.compare(password, user.password)) {
+        //C'est le bon mot de passe, on retourne l'utilisateur
+        return done(null, user)
+      } else {
+        return done(null, false, { message: 'Le mot de passe est invalide' })
+      }
+    } catch (e) {
+      return done(e)
+    }
+  }
+
+  passport.use(new LocalStrategy({ usernameField: 'username' }, authenticateUser))
+  passport.serializeUser((user, done) => done(null, user.id))
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id))
+  })
+}
+
+module.exports = initialize
---
+++ server.js
@@ -0,0 +1,117 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+//d
+/* app.engine('pug', require('pug').__express)
+app.set('views', path.join(__dirname, 'views')); */
+app.set("view engine", "pug");
+//f
+const initializePassport = require('./passport-config')
+initializePassport(
+  passport,
+  username => users.find(user => user.username === username),
+  id => users.find(user => user.id === id)
+)
+
+let users = []
+
+app.set('view-engine', 'ejs')
+app.use(express.urlencoded({
+  extended: false
+}))
+app.use(flash())
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
+
+app.get('/', verifieAuthentification, (req, res) => {
+  res.render('accueil.pug', {
+    username: req.user.username
+  })
+})
+
+app.get('/login', verifiePasAuthentification, (req, res) => {
+  res.render('login.pug')
+})
+
+app.post('/login', verifiePasAuthentification, passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/login',
+  failureFlash: true
+}))
+
+app.get('/register', verifiePasAuthentification, (req, res) => {
+  res.render('register.pug')
+})
+
+app.get('/admin', verifiePasAuthentificationAdmin, (req, res) => {
+  res.render('admin.pug')
+})
+
+
+app.post('/register', verifiePasAuthentification, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      username: req.body.username,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
+})
+
+app.delete('/logout', (req, res) => {
+  req.session.destroy();
+  
+  for( let i = 0; i < users.length; i++){
+    if ( users[i].username === req.user.username) {
+      users.splice(i, 1); //pour supprimer tous l'utilisateur courant
+    }
+  }
+  
+  req.user = null;
+  //users = []; //pour supprimer tous les utilisateurs
+  req.logOut();
+  res.redirect('/login')
+})
+
+function verifieAuthentification(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/login')
+}
+
+function verifiePasAuthentification(req, res, next) {
+  if (req.isAuthenticated()) {
+    return res.redirect('/')
+  }
+  next()
+}
+
+function verifiePasAuthentificationAdmin(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next()
+  }
+
+  res.redirect('/admin')
+}
+
+app.listen(3000)
