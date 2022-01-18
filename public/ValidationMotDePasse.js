8c6eda77801f2ec0c326aa2d16384c80bcd2bc40
Benoit <b.robichaud12@gmail.com>
2022-01-18 15:37:32 -0500

    mise en page login et register

--- public/ValidationMotDePasse.js
+++ public/ValidationMotDePasse.js
@@ -1,40 +1,40 @@
-
-function validatePassword(password,errorText,formSubmit){
-    
-   let InputValue = document.getElementById(password).value;
-   let regex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*»/?()])(?=.{8,})");
-   let regexSpecial = new RegExp("^(?=.*[!@#\$%\^&\*»/?()])");
-   let errors = [];
+function validatePassword(password, errorText, formSubmit) {
+  let InputValue = document.getElementById(password).value;
+  let regex = new RegExp(
+    "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*»/?()])(?=.{8,})"
+  );
+  let regexSpecial = new RegExp("^(?=.*[!@#$%^&*»/?()])");
+  let errors = [];
   // document.getElementById(passwordText).innerHTML = "Password value:- "+InputValue;
 
-    if (InputValue.length < 8) {
-        errors.push("Le mot de passe doit avoir au moins 8 caractères."); 
-    }
-    if (InputValue.search(/[a-z]/) < 0) {
-        errors.push("Le mot de passe doit avoir au moins une lettre minuscule.");
-    }
-    if (InputValue.search(/[A-Z]/) < 0) {
-        errors.push("Le mot de passe doit avoir au moins une lettre majuscule.");
-    }
-    if (InputValue.search(/[0-9]/) < 0) {
-        errors.push("Le mot de passe doit avoir au moins un chiffre."); 
-    }
-    if (!regexSpecial.test(InputValue)) {
-        errors.push("Le mot de passe doit avoir au moins un caractère spécial."); 
-    }
-    if (errors.length > 0) {
-        document.getElementById(errorText).innerHTML = errors.join("<br/>");
-        return false;
-    }
+  if (InputValue.length < 8) {
+    errors.push("Le mot de passe doit avoir au moins 8 caractères.");
+  }
+  if (InputValue.search(/[a-z]/) < 0) {
+    errors.push("Le mot de passe doit avoir au moins une lettre minuscule.");
+  }
+  if (InputValue.search(/[A-Z]/) < 0) {
+    errors.push("Le mot de passe doit avoir au moins une lettre majuscule.");
+  }
+  if (InputValue.search(/[0-9]/) < 0) {
+    errors.push("Le mot de passe doit avoir au moins un chiffre.");
+  }
+  if (!regexSpecial.test(InputValue)) {
+    errors.push("Le mot de passe doit avoir au moins un caractère spécial.");
+  }
+  if (errors.length > 0) {
+    document.getElementById(errorText).innerHTML = errors.join("<br/>");
+    return false;
+  }
 
-   if(!regex.test(InputValue)) {
-        document.getElementById(errorText).innerHTML = "Invalid Password";
-        return false;
-   } else{
-        document.getElementById(errorText).innerHTML = "";
-        document.getElementById(formSubmit).submit();
-        return true;
-   }
+  if (!regex.test(InputValue)) {
+    document.getElementById(errorText).innerHTML = "Invalid Password";
+    return false;
+  } else {
+    document.getElementById(errorText).innerHTML = "";
+    document.getElementById(formSubmit).submit();
+    return true;
+  }
 }
 
 /*
@@ -67,7 +67,7 @@ function validatePassword(){
      }
 }
 */
- /*sdSD14$47&%@
+/*sdSD14$47&%@
 function validatePassword(){
     
    let InputValue = $("#password").val();
